a46cb89b0c78f63c2bbee0946d513971e7ead93b
Susan Nio <susan.wen88@gmail.com>
2018-07-16 17:14:37 +0700

    Adding index.js

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 "use strict"
+class Animal{
+    constructor(numlegs, is_warm_blooded,is_herbivore){
+        this.num_legs = 2
+        this.is_warm_blooded = true
+        this.is_herbivore = true
+        this.superpower = new SuperPower();
+    }
+    
+}
+
+class Frog extends Animal {
+    constructor(){
+        super()
+            this.num_legs = 4
+            this.is_warm_blooded = false
+            this.is_herbivore = false
+    }
+}
+
+class Duck extends Animal {
+    constructor(){
+       super()
+    }
+}
+
+class Fox extends Animal {
+    constructor(){
+        super()
+        this.num_legs = 4
+        this.is_herbivore = false
+    }
+}
+class SuperPower{
+    constructor(){
+
+    }
+
+    use_laser_vision(){
+        console.log("Have laser vision");
+    }
+
+    be_invisible(){
+        console.log("like a ninja");
+    }
+
+    super_swimmer(){
+        console.log("like a tornado");
+    }
+
+}
+
+var fox = new Fox();
+var frog = new Frog();
+var duck = new Duck();
+fox.superpower.use_laser_vision();//Have laser vision
+frog.superpower.super_swimmer();//like a tornado
+console.log(duck.num_legs);//2
